860b82483cc6537efa7785d1f14b3ff55b85245c
George Higbie <ghigbie@monigle.com>
2019-03-21 09:52:36 -0600

    Import templates

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,21 @@ title = "Modular Flask"
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template('home.html', title=title, user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+                {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('home.html', title=title, user=user, posts=posts)
 
 # @app.route('/about')
 # def index():
