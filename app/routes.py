cbb8290e4be5eb5b9a74ea8d55e7524e0d44b856
George Higbie <ghigbie@monigle.com>
2019-03-21 09:31:51 -0600

    Add not found page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import app
 from flask import render_template
 
+title = "Modular Flask"
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', title=title)
 
+@app.route('/about')
+def index():
+    return render_template('about.html', title=title)
